83bf06f5b74b8de1a8d77be0238d9c4748612c5d
twozeronine <amdusias209@gmail.com>
2021-11-26 00:19:39 +0900

    plugin : ExcelToJson 추가

---
+++ Assets/ResourcesSources/Datas/Excel2Json.config.js
@@ -0,0 +1,4 @@
+var g_sourceFolder = g_scriptFolder + "DataTableExcel";
+var g_targetFolder = "../../../Resources/Data"; // subdirectory in g_sourceFolder
+var g_tempSuffix = ".$$$";
+var g_prettyOutput = true; // false for compact
