d6d01dda6cb2386af6dc2b5ba31c4e2452108383
twozeronine <amdusias209@gmail.com>
2021-11-28 12:39:43 +0900

    feat : 타일 랜덤 섞기 이후에 매칭 할 블럭이 있나 검사하는 로직

--- Assets/Scripts/Contents/Core/HexaBlastEngine.cs
+++ Assets/Scripts/Contents/Core/HexaBlastEngine.cs
@@ -50,6 +50,7 @@ public static class HexaBlastEngine
                                  matchedBlock.IsMatched3TopRight).ToList();
     }
 
+    // 블럭을 셔플하는 로직
     public static void ShuffledMatched3Blocks(ref Blocks preBlocks,List<MatchedBlock> mustShuffledBlocks)
     {
         foreach (var mustShuffledBlock in mustShuffledBlocks)
@@ -59,6 +60,80 @@ public static class HexaBlastEngine
             preBlocks.BlocksMap[nextBlock.BlockPos].Color = nextBlock.Color;
         }
     }
+    
+    // 제거 할 수 있는 블럭이 있는지 빠르게 체크하는 로직
+    // 다음에 힌트 검사 로직은 특수블럭, 5,4개만 검사함.
+    public static bool TryCheckCanRemoveBlocks(Blocks preBlocks, out Block hintBlock)
+    {
+        var result = (from preBlock in preBlocks.BlocksMap.Values
+            where
+                TryCheckMatch3(preBlock, preBlocks, Direction.Top) ||
+                TryCheckMatch3(preBlock, preBlocks, Direction.Bottom) ||
+                TryCheckMatch3(preBlock, preBlocks, Direction.TopRight) ||
+                TryCheckMatch3(preBlock, preBlocks, Direction.BottomRight) ||
+                TryCheckMatch3(preBlock, preBlocks, Direction.TopLeft) ||
+                TryCheckMatch3(preBlock, preBlocks, Direction.BottomLeft) 
+            select preBlock).ToList();
+
+        hintBlock = result.FirstOrDefault();
+        return result.Any();
+    }
+    
+    public static bool TryCheckMatch3(Block block, Blocks blocks, Direction scanDir)
+    {
+        var nextBlock = HexaBlastEngineUtil.GetBlock(blocks, scanDir, block.BlockPos);
+        if (nextBlock == null) return false;
+
+        // 가상으로 블록을 서로 교환한 상태로 만듬.
+        var nextBlocks = new Blocks()
+        {
+            BlocksMap = new Dictionary<Vector2Int, Block>(blocks.BlocksMap)
+            {
+                [nextBlock.BlockPos] = new Block()
+                {
+                    BlockPos = block.BlockPos,
+                    BlockType = block.BlockType,
+                    Color = block.Color,
+                    ParentTile = block.ParentTile,
+                    IsValid = block.IsValid,
+                },
+                [block.BlockPos] = new Block()
+                {
+                    BlockPos = nextBlock.BlockPos,
+                    BlockType = block.BlockType,
+                    Color = nextBlock.Color,
+                    ParentTile = nextBlock.ParentTile,
+                    IsValid = nextBlock.IsValid,
+                }
+            }
+        };
+
+        nextBlocks.BlocksMap[nextBlock.BlockPos].BlockPos = nextBlock.BlockPos;
+
+        // 가상으로 교환한 방향의 반대 방향으로의 검색은 하지않음
+        var exceptDirection = scanDir switch
+        {
+            Direction.Top => Direction.Bottom,
+            Direction.Bottom => Direction.Top,
+            Direction.TopLeft => Direction.BottomRight,
+            Direction.TopRight => Direction.BottomLeft,
+            Direction.BottomLeft => Direction.TopRight,
+            Direction.BottomRight => Direction.TopLeft,
+            _ => throw new ArgumentOutOfRangeException(nameof(scanDir), scanDir, null)
+        };
+
+        var result = (from direction in Enum.GetValues(typeof(Direction))
+                .Cast<Direction>()
+            where !direction.Equals(exceptDirection)
+            select CheckMatch3Pattern(nextBlocks.BlocksMap[nextBlock.BlockPos], nextBlocks, direction) ||
+                   CheckMatch3MiddleY(nextBlocks.BlocksMap[nextBlock.BlockPos], nextBlocks) ||
+                   CheckMatch3MiddleDiagonalLeftDown(nextBlocks.BlocksMap[nextBlock.BlockPos], nextBlocks) ||
+                   CheckMatch3MiddleDiagonalRightDown(nextBlocks.BlocksMap[nextBlock.BlockPos], nextBlocks)
+                   ).ToList();
+
+        return result.Any(isTrue => isTrue);
+    }
+
     
     // 매치 3가 되었나 검사 하는 로직
     public static bool CheckMatch3Pattern(Block block, Blocks blocksMap, Direction dir, int depth = 0,
@@ -74,6 +149,51 @@ public static class HexaBlastEngine
         }
 
         return depth >= 2 + matchCountNumber;
+    }
+    
+    /*  Pattern 1 
+     *  1 
+     *  1 <-- 이쪽 블럭이 움직여져서 매치3 가 된 경우 탐지
+     *  1 
+     *  하는 로직
+     */
+    public static bool CheckMatch3MiddleY(Block block, Blocks blocksMap)
+    {
+        var nextTopBlock = HexaBlastEngineUtil.GetBlock(blocksMap, Direction.Top, block.BlockPos);
+        var nextBottomBlock = HexaBlastEngineUtil.GetBlock(blocksMap, Direction.Bottom, block.BlockPos);
+
+        return nextTopBlock is { IsValid: true } && nextTopBlock.Color.Equals(block.Color) &&
+               nextBottomBlock is { IsValid: true } && nextBottomBlock.Color.Equals(block.Color);
+    }
+    
+    /*  Pattern 2 
+     *  1 
+     *    1 <-- 이쪽 블럭이 움직여져서 매치3 가 된 경우 탐지
+     *      1 
+     *  하는 로직
+     */
+    public static bool CheckMatch3MiddleDiagonalRightDown(Block block, Blocks blocksMap)
+    {
+        var nextTopLeftBlock = HexaBlastEngineUtil.GetBlock(blocksMap, Direction.TopLeft, block.BlockPos);
+        var nextBottomRightBlock = HexaBlastEngineUtil.GetBlock(blocksMap, Direction.BottomRight, block.BlockPos);
+
+        return nextTopLeftBlock is { IsValid: true } && nextTopLeftBlock.Color.Equals(block.Color) &&
+               nextBottomRightBlock is { IsValid: true } && nextBottomRightBlock.Color.Equals(block.Color);
+    }
+    
+    /*  Pattern 3 
+     *      1
+     *    1 <-- 이쪽 블럭이 움직여져서 매치3 가 된 경우 탐지
+     *  1 
+     *  하는 로직
+     */
+    public static bool CheckMatch3MiddleDiagonalLeftDown(Block block, Blocks blocksMap)
+    {
+        var nextTopRightBlock = HexaBlastEngineUtil.GetBlock(blocksMap, Direction.TopRight, block.BlockPos);
+        var nextBottomLeftBlock = HexaBlastEngineUtil.GetBlock(blocksMap, Direction.BottomLeft, block.BlockPos);
+
+        return nextTopRightBlock is { IsValid: true } && nextTopRightBlock.Color.Equals(block.Color) &&
+               nextBottomLeftBlock is { IsValid: true } && nextBottomLeftBlock.Color.Equals(block.Color);
     }
 }
 
--- Assets/Scripts/Contents/Game/Model/GameBlocksModel.cs
+++ Assets/Scripts/Contents/Game/Model/GameBlocksModel.cs
@@ -12,25 +12,46 @@ public class GameBlocksModel : BaseMVP.IModel
     {
         Managers.Game.GameBlocksModel = this;
 
-        var initStageBlocks = HexaBlastEngine.InitBlock(Managers.Game.GameTilesModel.TilesProperty.Value);
+        Blocks initStageBlocks;
+        var isFinishInitBlocks = false;
 
-        var completeBuildStageInitBlocks = false;
-
-        var count = 0;
-        while (!completeBuildStageInitBlocks)
+        var escapeCount = 0; // 로직에 오류가 있을 시 비상 탈출 카운트
+        do
         {
-            var match3BlockList = HexaBlastEngine.ScanInitMatched3Blocks(initStageBlocks);
+            initStageBlocks = HexaBlastEngine.InitBlock(Managers.Game.GameTilesModel.TilesProperty.Value);
 
-            if (match3BlockList.Any())
+            var completeBuildStageInitBlocks = false;
+
+            // 초기 설정된 블럭중 매치 3 가 된 블럭이 있는지 검사하고 있으면 섞어 주는 로직
+            var count = 0;
+            while (!completeBuildStageInitBlocks)
             {
-                HexaBlastEngine.ShuffledMatched3Blocks(ref initStageBlocks,match3BlockList);
+                var match3BlockList = HexaBlastEngine.ScanInitMatched3Blocks(initStageBlocks);
+
+                if (match3BlockList.Any())
+                {
+                    HexaBlastEngine.ShuffledMatched3Blocks(ref initStageBlocks, match3BlockList);
+                }
+                else
+                {
+                    completeBuildStageInitBlocks = true;
+                }
+
+                Debug.Log(count + "번 섞음");
+                count++;
             }
-            else
-            {
-                completeBuildStageInitBlocks = true;    
-            }
-            Debug.Log(count+"번 섞음");
-            count++;
+
+            // 섞어서 나온 블럭에서 더이상 제거 할 블럭이 없는지 검사하는 로직
+            isFinishInitBlocks = HexaBlastEngine.TryCheckCanRemoveBlocks(initStageBlocks, out var hintBlock);
+            Debug.Log("탈출 :" + isFinishInitBlocks);
+            escapeCount++;
+            // 로직에 오류가 있을 시 비상 탈출문
+            if (escapeCount == 100) break;
+        } while (!isFinishInitBlocks);
+
+        foreach (var tilesMapValue in Managers.Game.GameTilesModel.TilesProperty.Value.TilesMap.Values)
+        {
+            tilesMapValue.ChildBlock = initStageBlocks.GetBlock(tilesMapValue.TilePos);
         }
         
         BlocksProperty.Value = initStageBlocks;
